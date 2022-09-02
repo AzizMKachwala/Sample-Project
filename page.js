a2b07f7e5553944505d90fb096bd85adfa19f219
Aziz M Kachwala <69758420+AzizMKachwala@users.noreply.github.com>
2022-09-03 01:39:01 +0530

    Delete page.js

--- page.js
+++
@@ -1,7 +0,0 @@
-function save() {
-    alert("Music Saved For Later...");
-}
-function play(){
-    var audio = new Audio('C:\Users\Aziz Kachwala\Desktop\practical test\UtahCase.mp3');  
-    audio.play();
-}
