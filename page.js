eee06a2412d1ee5ae58bf7f430a921b9984cf911
Aziz M Kachwala <69758420+AzizMKachwala@users.noreply.github.com>
2022-09-02 22:55:10 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,7 @@
+function save() {
+    alert("Music Saved For Later...");
+}
+function play(){
+    var audio = new Audio('C:\Users\Aziz Kachwala\Desktop\practical test\UtahCase.mp3');  
+    audio.play();
+}
